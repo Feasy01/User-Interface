13776230760cb3f98885e2ffe5a3ac6a56517ce6
Szymon_walczak <ext.swalczak@pesa.pl>
2021-09-02 12:39:40 +0200

    added option to delete marks, add labels

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.graphics.vertex_instructions import (Line, Rectangle)
 from kivy.graphics.context_instructions import Color
+from kivy.graphics.instructions import InstructionGroup
 from kivy.clock import Clock
 from datetime import datetime
 import itertools
@@ -30,7 +32,7 @@ class ImageButton(ButtonBehavior, Image):
         img = Image(source = self.source, allow_stretch=True , keep_ratio=False, size_hint=(1,1))
         # self.parent.parent.parent.parent.ids.layout.remove_widget()
         # self.parent.parent.parent.parent.ids.layout.add_widget(img)
-        self.parent.parent.parent.parent.ids.layout.ids.photo.source=self.source
+        self.parent.parent.parent.ids.layout.ids.photo.source=self.source
         # print(img.source)
 
         # self.ids.layout.add_widget(photo)
@@ -46,12 +48,27 @@ class butt(Button):
         super(butt, self).__init__(**kwargs)
         self.numbers = numbers
         self.coords = coords
+    def find_index(self):
+        for i , button in enumerate(self.parent.parent.parent.parent.ids.layout.ids.photo.buttons):
+            if button.numbers == self.numbers:
+                return i
+
+
     def on_press(self):
         print(self.numbers)
         line = Line(points = self.coords, width=3)
-        self.parent.parent.parent.parent.ids.layout.ids.photo.canvas.remove(line)
-        print(self.parent.parent.parent.parent.ids.layout.ids.photo.canvas.children)
-
+        # self.parent.parent.parent.parent.ids.layout.ids.photo.canvas.clear()
+        self.parent.parent.parent.parent.ids.layout.ids.photo.canvas.remove(self.parent.parent.parent.parent.ids.layout.ids.photo.instructions[self.find_index()])
+        # print(self.parent.ids)
+        #     # (self.parent.parent.parent.parent.ids.photo.instructions[self.find_index()])
+        self.parent.parent.parent.parent.ids.layout.ids.photo.table.pop(self.find_index())
+        self.parent.parent.parent.parent.ids.layout.ids.photo.instructions.pop(self.find_index())
+        print(self.parent.parent.parent.parent.ids.layout.ids.photo.table)
+        print(self.parent.parent.parent.parent.ids.layout.ids.photo.buttons)
+        print(self.parent.parent.parent.parent.ids.layout.ids.photo.instructions)
+        self.parent.parent.parent.parent.ids.layout.ids.photo.buttons.pop(self.find_index())
+        self.parent.parent.parent.parent.ids.objects.remove_widget(self)
+        # self.parent.remove_widget()
 
 
 class Frame():
@@ -60,32 +77,35 @@ class Frame():
         self.coords = coords
         self.name = name
         self.id = next(self.id_iter)
+
+
 class Draw(Image):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.line = [0,0,0,0,0,0,0,0,0,0]
         self.table=[]
+        self.instructions = []
+        self.buttons = []
+
+
     def remove(self,number):
         item = Line(points=self.table[number].coords)
-        with self.canvas:
-            self.canvas.remove(item)
+        print(self.canvas.get_group(Line))
         del self.table[number]
+
+
     def on_touch_down(self,touch):
         # if touch.x
-        print(self.to_parent(touch.x,touch.y))
-        print(self.pos)
-        print(self.size)
         if (touch.x > self.pos[0] and touch.x< self.pos[0]+self.size[0]):
             self.line[0] = touch.x
             self.line[1] = touch.y
-            for i in self.table:
-                self.canvas.remove(Line(points=i.coords))
             self.table.append(Frame(self.line,"name"))
-            new_widget = butt(text=str(self.table[-1].id),size_hint=(1,None),width=300, numbers = self.table[-1].id, coords = self.table[-1].coords)
-            self.parent.parent.ids.objects.add_widget(new_widget)
+            self.buttons.append(butt(text=str(self.table[-1].id),size_hint=(1,None),width=300, numbers = self.table[-1].id, coords = self.table[-1].coords))
+            self.parent.parent.ids.objects.add_widget(self.buttons[-1])
             with self.canvas:
                 Color(0,0,1,1)
                 # touch.ud["line"]=Line(points=(touch.x,touch.y,touch.x,touch.y,touch.x,touch.y,touch.x,touch.y), closed =True)
+
 
     def on_touch_move(self, touch):
         if (touch.x > self.pos[0] and touch.x < self.pos[0] + self.size[0]):
@@ -99,11 +119,13 @@ class Draw(Image):
             self.line[9] = self.line[1]
             # with self.canvas:
             #     Line(points = self.line, closed=True)
+
+
     def on_touch_up(self, touch):
         if (touch.x > self.pos[0] and touch.x < self.pos[0] + self.size[0]):
-            for i in self.table:
-                with self.canvas:
-                    Line(points=i.coords, width=3)
+            if self.table:
+                self.instructions.append(Line(points=self.table[-1].coords))
+                self.canvas.add(self.instructions[-1])
             print(self.table)
         #dodajemy wspolrzedne do tablicy jakiejs ta se tak o
                 # touch.ud["line"].points[2] = touch.x
@@ -113,25 +135,22 @@ class Draw(Image):
                 # print(touch.ud["line"].points)
                 # touch.ud["line"].points[6] = touch.x
                 # touch.ud["line"].points[7] = touch.y
+
+
 class MainWidget(BoxLayout):
     app = App.get_running_app()
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.names = []
+        self.current_calss = ""
     def selected(self,removeFiles):
         self.add(removeFiles)
-
-    def draw(self,points):
-        with self.canvas:
-            Color(0,1,0,1)
-            Line(points=points,
-                 close=True,
-                 width = 3)
-
     def getClass(self,text):
         self.names.append(text)
-        label = Label(text=text, size_hint=(1,None),height=30)
-        self.ids.classes.add_widget(label)
+        self.ids.list3.values.append(text)
+    def spinner_clicked(self,text):
+        self.current_calss = text
+        print(self.current_calss)
 
 
     def getPoints(self):
@@ -142,11 +161,10 @@ class MainWidget(BoxLayout):
         # img = Image(source = '' , allow_stretch=True , keep_ratio=False, size_hint=(1,1))
         # self.ids.layout.add_widget(img)
         # self.ids.layout.ids['photo'] = weakref.ref(img)
-        rectangle = Draw(source='',allow_stretch=True , keep_ratio=False, size_hint=(1,1))
-        self.ids.layout.add_widget(rectangle)
-        self.ids.layout.ids['photo'] = weakref.ref(rectangle)
+        picture = Draw(source='',allow_stretch=True , keep_ratio=False, size_hint=(1,1))
+        self.ids.layout.add_widget(picture)
+        self.ids.layout.ids['photo'] = weakref.ref(picture)
         # self.draw()
-        print(self.ids.layout.ids)
         # img = Image(source='')
         # self.ids.layout.add_widget(img)
     # def btn(self):
@@ -154,7 +172,6 @@ class MainWidget(BoxLayout):
     #     print("lessgo")
     #     self.remove_widget(self)
     def append(self,source):
-        print(source)
         photo = Image(source=source)
         self.ids.layout.add_widget(photo)
     def press(self):
@@ -162,7 +179,6 @@ class MainWidget(BoxLayout):
         print(photo.source)
         self.ids.layout.add_widget(photo)
     def add(self,list):
-        print(list)
         for i,source in enumerate(list):
             try:
                 a = ImageButton(source=source , allow_stretch=True , keep_ratio=False, size_hint=(0.25,None))
